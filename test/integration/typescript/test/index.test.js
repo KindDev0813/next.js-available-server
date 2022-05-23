KindDev0813/next.js-available-server

commit 21a3e80129f4b47079e5c92e5078b2295dd996d8

perf: run type check and eslint in parallel (#37105)

packages/next/build/index.ts
@@ -4,6 +4,7 @@ import chalk from 'next/dist/compiled/chalk'
 import crypto from 'crypto'
 import { isMatch } from 'next/dist/compiled/micromatch'
 import { promises, writeFileSync } from 'fs'
+import { Worker as JestWorker } from 'next/dist/compiled/jest-worker'
 import { Worker } from '../lib/worker'
 import devalue from 'next/dist/compiled/devalue'
 import { escapeStringRegexp } from '../shared/lib/escape-regexp'
@@ -34,7 +35,6 @@ import { nonNullable } from '../lib/non-nullable'
 import { recursiveDelete } from '../lib/recursive-delete'
 import { verifyAndLint } from '../lib/verifyAndLint'
 import { verifyPartytownSetup } from '../lib/verify-partytown-setup'
-import { verifyTypeScriptSetup } from '../lib/verifyTypeScriptSetup'
 import {
   BUILD_ID_FILE,
   BUILD_MANIFEST,
@@ -229,30 +229,74 @@ export default async function build(
       const ignoreTypeScriptErrors = Boolean(
         config.typescript.ignoreBuildErrors
       )
-      const typeCheckStart = process.hrtime()
-      const typeCheckingSpinner = createSpinner({
-        prefixText: `${Log.prefixes.info} ${
-          ignoreTypeScriptErrors
-            ? 'Skipping validation of types'
-            : 'Checking validity of types'
-        }`,
-      })
 
-      const verifyResult = await nextBuildSpan
-        .traceChild('verify-typescript-setup')
-        .traceAsyncFn(() =>
+      const ignoreESLint = Boolean(config.eslint.ignoreDuringBuilds)
+      const eslintCacheDir = path.join(cacheDir, 'eslint/')
+      const shouldLint = !ignoreESLint && runLint
+
+      if (ignoreTypeScriptErrors) {
+        Log.info('Skipping validation of types')
+      }
+      if (runLint && ignoreESLint) {
+        // only print log when build requre lint while ignoreESLint is enabled
+        Log.info('Skipping linting')
+      }
+
+      let typeCheckingAndLintingSpinnerPrefixText: string | undefined
+      let typeCheckingAndLintingSpinner:
+        | ReturnType<typeof createSpinner>
+        | undefined
+
+      if (!ignoreTypeScriptErrors && shouldLint) {
+        typeCheckingAndLintingSpinnerPrefixText =
+          'Linting and checking validity of types'
+      } else if (!ignoreTypeScriptErrors) {
+        typeCheckingAndLintingSpinnerPrefixText = 'Checking validity of types'
+      } else if (shouldLint) {
+        typeCheckingAndLintingSpinnerPrefixText = 'Linting'
+      }
+
+      // we will not create a spinner if both ignoreTypeScriptErrors and ignoreESLint are
+      // enabled, but we will still verifying project's tsconfig and dependencies.
+      if (typeCheckingAndLintingSpinnerPrefixText) {
+        typeCheckingAndLintingSpinner = createSpinner({
+          prefixText: `${Log.prefixes.info} ${typeCheckingAndLintingSpinnerPrefixText}`,
+        })
+      }
+
+      const typeCheckStart = process.hrtime()
+
+      const [[verifyResult, typeCheckEnd]] = await Promise.all([
+        nextBuildSpan.traceChild('verify-typescript-setup').traceAsyncFn(() =>
           verifyTypeScriptSetup(
             dir,
             [pagesDir, viewsDir].filter(Boolean) as string[],
             !ignoreTypeScriptErrors,
             config,
-            cacheDir
-          )
-        )
+            cacheDir,
+            config.experimental.cpus,
+            config.experimental.workerThreads
+          ).then((resolved) => {
+            const checkEnd = process.hrtime(typeCheckStart)
+            return [resolved, checkEnd] as const
+          })
+        ),
+        shouldLint &&
+          nextBuildSpan.traceChild('verify-and-lint').traceAsyncFn(async () => {
+            await verifyAndLint(
+              dir,
+              eslintCacheDir,
+              config.eslint?.dirs,
+              config.experimental.cpus,
+              config.experimental.workerThreads,
+              telemetry
+            )
+          }),
+      ])
 
-      const typeCheckEnd = process.hrtime(typeCheckStart)
+      typeCheckingAndLintingSpinner?.stopAndPersist()
 
-      if (!ignoreTypeScriptErrors) {
+      if (!ignoreTypeScriptErrors && verifyResult) {
         telemetry.record(
           eventTypeCheckCompleted({
             durationInSeconds: typeCheckEnd[0],
@@ -264,27 +308,6 @@ export default async function build(
         )
       }
 
-      if (typeCheckingSpinner) {
-        typeCheckingSpinner.stopAndPersist()
-      }
-
-      const ignoreESLint = Boolean(config.eslint.ignoreDuringBuilds)
-      const eslintCacheDir = path.join(cacheDir, 'eslint/')
-      const shouldLint = !ignoreESLint && runLint
-      if (shouldLint) {
-        await nextBuildSpan
-          .traceChild('verify-and-lint')
-          .traceAsyncFn(async () => {
-            await verifyAndLint(
-              dir,
-              eslintCacheDir,
-              config.eslint?.dirs,
-              config.experimental.cpus,
-              config.experimental.workerThreads,
-              telemetry
-            )
-          })
-      }
       const buildLintEvent: EventBuildFeatureUsage = {
         featureName: 'build-lint',
         invocationCount: shouldLint ? 1 : 0,
@@ -2316,6 +2339,50 @@ export default async function build(
     await flushAllTraces()
     teardownTraceSubscriber()
   }
+}
+
+/**
+ * typescript will be loaded in "next/lib/verifyTypeScriptSetup" and
+ * then passed to "next/lib/typescript/runTypeCheck" as a parameter.
+ *
+ * Since it is impossible to pass a function from main thread to a worker,
+ * instead of running "next/lib/typescript/runTypeCheck" in a worker,
+ * we will run entire "next/lib/verifyTypeScriptSetup" in a worker instead.
+ */
+function verifyTypeScriptSetup(
+  dir: string,
+  intentDirs: string[],
+  typeCheckPreflight: boolean,
+  config: NextConfigComplete,
+  cacheDir: string | undefined,
+  numWorkers: number | undefined,
+  enableWorkerThreads: boolean | undefined
+) {
+  const typeCheckWorker = new JestWorker(
+    require.resolve('../lib/verifyTypeScriptSetup'),
+    {
+      numWorkers,
+      enableWorkerThreads,
+    }
+  ) as JestWorker & {
+    verifyTypeScriptSetup: typeof import('../lib/verifyTypeScriptSetup').verifyTypeScriptSetup
+  }
+
+  typeCheckWorker.getStdout().pipe(process.stdout)
+  typeCheckWorker.getStderr().pipe(process.stderr)
+
+  return typeCheckWorker
+    .verifyTypeScriptSetup(
+      dir,
+      intentDirs,
+      typeCheckPreflight,
+      config,
+      cacheDir
+    )
+    .then((result) => {
+      typeCheckWorker.end()
+      return result
+    })
 }
 
 function generateClientSsgManifest(

commit 346a3ccc1b40bc5c85af4939f888e03344b36429

fix(typescript): worker execution failed with custom `next.config.js` (#37125)
## Bug

- [x] Related issues linked using `fixes #number`
- [x] Integration tests added
- [ ] Errors have helpful link attached, see `contributing.md`

The PR fixes #37122, an issue introduced by #37105.

`next.config.js` might/will include functions (custom `webpack`, `generateBuildId`, `exportPathsMap`, etc.) that are not able to pass from the main thread to a worker. The PR fixes the issue by only passing primitive args to the worker.

packages/next/build/index.ts
@@ -272,7 +272,8 @@ export default async function build(
             dir,
             [pagesDir, viewsDir].filter(Boolean) as string[],
             !ignoreTypeScriptErrors,
-            config,
+            config.typescript.tsconfigPath,
+            config.images.disableStaticImages,
             cacheDir,
             config.experimental.cpus,
             config.experimental.workerThreads
@@ -2353,7 +2354,8 @@ function verifyTypeScriptSetup(
   dir: string,
   intentDirs: string[],
   typeCheckPreflight: boolean,
-  config: NextConfigComplete,
+  tsconfigPath: string,
+  disableStaticImages: boolean,
   cacheDir: string | undefined,
   numWorkers: number | undefined,
   enableWorkerThreads: boolean | undefined
@@ -2376,7 +2378,8 @@ function verifyTypeScriptSetup(
       dir,
       intentDirs,
       typeCheckPreflight,
-      config,
+      tsconfigPath,
+      disableStaticImages,
       cacheDir
     )
     .then((result) => {

packages/next/lib/typescript/getTypeScriptIntent.ts
@@ -1,6 +1,5 @@
 import { promises as fs } from 'fs'
 import path from 'path'
-import { NextConfigComplete } from '../../server/config-shared'
 import { fileExists } from '../file-exists'
 import { recursiveReadDir } from '../recursive-readdir'
 
@@ -9,18 +8,20 @@ export type TypeScriptIntent = { firstTimeSetup: boolean }
 export async function getTypeScriptIntent(
   baseDir: string,
   intentDirs: string[],
-  config: NextConfigComplete
+  tsconfigPath: string
 ): Promise<TypeScriptIntent | false> {
-  const tsConfigPath = path.join(baseDir, config.typescript.tsconfigPath)
+  const resolvedTsConfigPath = path.join(baseDir, tsconfigPath)
 
   // The integration turns on if we find a `tsconfig.json` in the user's
   // project.
-  const hasTypeScriptConfiguration = await fileExists(tsConfigPath)
+  const hasTypeScriptConfiguration = await fileExists(resolvedTsConfigPath)
   if (hasTypeScriptConfiguration) {
-    const content = await fs.readFile(tsConfigPath, { encoding: 'utf8' }).then(
-      (txt) => txt.trim(),
-      () => null
-    )
+    const content = await fs
+      .readFile(resolvedTsConfigPath, { encoding: 'utf8' })
+      .then(
+        (txt) => txt.trim(),
+        () => null
+      )
     return { firstTimeSetup: content === '' || content === '{}' }
   }
 

packages/next/lib/verifyTypeScriptSetup.ts
@@ -14,7 +14,6 @@ import { TypeCheckResult } from './typescript/runTypeCheck'
 import { writeAppTypeDeclarations } from './typescript/writeAppTypeDeclarations'
 import { writeConfigurationDefaults } from './typescript/writeConfigurationDefaults'
 import { missingDepsError } from './typescript/missingDependencyError'
-import { NextConfigComplete } from '../server/config-shared'
 
 const requiredPackages = [
   { file: 'typescript', pkg: 'typescript', exportsRestrict: false },
@@ -34,14 +33,15 @@ export async function verifyTypeScriptSetup(
   dir: string,
   intentDirs: string[],
   typeCheckPreflight: boolean,
-  config: NextConfigComplete,
+  tsconfigPath: string,
+  disableStaticImages: boolean,
   cacheDir?: string
 ): Promise<{ result?: TypeCheckResult; version: string | null }> {
-  const tsConfigPath = path.join(dir, config.typescript.tsconfigPath)
+  const resolvedTsConfigPath = path.join(dir, tsconfigPath)
 
   try {
     // Check if the project uses TypeScript:
-    const intent = await getTypeScriptIntent(dir, intentDirs, config)
+    const intent = await getTypeScriptIntent(dir, intentDirs, tsconfigPath)
     if (!intent) {
       return { version: null }
     }
@@ -68,17 +68,21 @@ export async function verifyTypeScriptSetup(
     }
 
     // Reconfigure (or create) the user's `tsconfig.json` for them:
-    await writeConfigurationDefaults(ts, tsConfigPath, intent.firstTimeSetup)
+    await writeConfigurationDefaults(
+      ts,
+      resolvedTsConfigPath,
+      intent.firstTimeSetup
+    )
     // Write out the necessary `next-env.d.ts` file to correctly register
     // Next.js' types:
-    await writeAppTypeDeclarations(dir, !config.images.disableStaticImages)
+    await writeAppTypeDeclarations(dir, !disableStaticImages)
 
     let result
     if (typeCheckPreflight) {
       const { runTypeCheck } = require('./typescript/runTypeCheck')
 
       // Verify the project passes type-checking before we go to webpack phase:
-      result = await runTypeCheck(ts, dir, tsConfigPath, cacheDir)
+      result = await runTypeCheck(ts, dir, resolvedTsConfigPath, cacheDir)
     }
     return { result, version: ts.version }
   } catch (err) {

packages/next/server/dev/next-dev-server.ts
@@ -424,7 +424,8 @@ export default class DevServer extends Server {
       this.dir,
       [this.pagesDir!, this.viewsDir].filter(Boolean) as string[],
       false,
-      this.nextConfig
+      this.nextConfig.typescript.tsconfigPath,
+      this.nextConfig.images.disableStaticImages
     )
 
     this.customRoutes = await loadCustomRoutes(this.nextConfig)

test/integration/typescript/test/index.test.js
@@ -108,6 +108,29 @@ export default function EvilPage(): JSX.Element {
     expect(output.code).toBe(0)
   })
 
+  it('should build the app with functions in next.config.js', async () => {
+    const nextConfig = new File(join(appDir, 'next.config.js'))
+
+    nextConfig.write(`
+    module.exports = {
+      webpack(config) { return config },
+      onDemandEntries: {
+        // Make sure entries are not getting disposed.
+        maxInactiveAge: 1000 * 60 * 60,
+      },
+    }
+    `)
+
+    try {
+      const output = await nextBuild(appDir, [], { stdout: true })
+
+      expect(output.stdout).toMatch(/Compiled successfully/)
+      expect(output.code).toBe(0)
+    } finally {
+      nextConfig.restore()
+    }
+  })
+
   it('should not inform when using default tsconfig path', async () => {
     const output = await nextBuild(appDir, [], { stdout: true })
     expect(output.stdout).not.toMatch(/Using tsconfig file:/)