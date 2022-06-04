KindDev0813/next.js-available-server

commit ebe770f890b59c7ee2caee3ed92decee79a081e6

Change experimental `layout=raw` to use native img lazy loading (#36985)
This PR changes the experimental `layout=raw` images to use the native lazy loading behavior (as opposed to the IntersectionObserver).

This will (eventually) lead to smaller client bundles and faster image loading since there is no JS needed to load the image.

However, we'll lose the `lazyRoot` and `lazyBoundary` behavior since those are specific to the IntersectionObserver implementation.

packages/next/client/image.tsx
@@ -372,7 +372,7 @@ export default function Image({
   priority = false,
   loading,
   lazyRoot = null,
-  lazyBoundary = '200px',
+  lazyBoundary,
   className,
   quality,
   width,
@@ -464,10 +464,10 @@ export default function Image({
   const [setIntersection, isIntersected, resetIntersected] =
     useIntersection<HTMLImageElement>({
       rootRef: lazyRoot,
-      rootMargin: lazyBoundary,
+      rootMargin: lazyBoundary || '200px',
       disabled: !isLazy,
     })
-  const isVisible = !isLazy || isIntersected
+  const isVisible = !isLazy || isIntersected || layout === 'raw'
 
   const wrapperStyle: JSX.IntrinsicElements['span']['style'] = {
     boxSizing: 'border-box',
@@ -566,10 +566,27 @@ export default function Image({
         `Image with src "${src}" has both "priority" and "loading='lazy'" properties. Only one should be used.`
       )
     }
-    if (layout === 'raw' && (objectFit || objectPosition)) {
-      throw new Error(
-        `Image with src "${src}" has "layout='raw'" and 'objectFit' or 'objectPosition'. For raw images, these and other styles should be specified using the 'style' attribute.`
-      )
+    if (layout === 'raw') {
+      if (objectFit) {
+        throw new Error(
+          `Image with src "${src}" has "layout='raw'" and "objectFit='${objectFit}'". For raw images, these and other styles should be specified using the "style" attribute.`
+        )
+      }
+      if (objectPosition) {
+        throw new Error(
+          `Image with src "${src}" has "layout='raw'" and "objectPosition='${objectPosition}'". For raw images, these and other styles should be specified using the "style" attribute.`
+        )
+      }
+      if (lazyRoot) {
+        throw new Error(
+          `Image with src "${src}" has "layout='raw'" and "lazyRoot='${lazyRoot}'". For raw images, native lazy loading is used so "lazyRoot" cannot be used.`
+        )
+      }
+      if (lazyBoundary) {
+        throw new Error(
+          `Image with src "${src}" has "layout='raw'" and "lazyBoundary='${lazyBoundary}'". For raw images, native lazy loading is used so "lazyBoundary" cannot be used.`
+        )
+      }
     }
     if (
       sizes &&
@@ -882,7 +899,7 @@ const ImageElement = ({
   blurStyle,
   isLazy,
   placeholder,
-  loading,
+  loading = 'lazy',
   srcString,
   config,
   unoptimized,
@@ -904,6 +921,8 @@ const ImageElement = ({
         decoding="async"
         data-nimg={layout}
         className={className}
+        // @ts-ignore - TODO: upgrade to `@types/react@17`
+        loading={layout === 'raw' ? loading : undefined}
         style={{ ...imgStyle, ...blurStyle }}
         ref={useCallback(
           (img: ImgElementWithDataProp) => {
@@ -974,7 +993,7 @@ const ImageElement = ({
             style={imgStyle}
             className={className}
             // @ts-ignore - TODO: upgrade to `@types/react@17`
-            loading={loading || 'lazy'}
+            loading={loading}
           />
         </noscript>
       )}

test/integration/image-component/default/test/index.test.js
@@ -713,6 +713,9 @@ function runTests(mode) {
       expect(await browser.elementById('raw1').getAttribute('srcset')).toBe(
         `/_next/image?url=%2Fwide.png&w=1200&q=75 1x, /_next/image?url=%2Fwide.png&w=3840&q=75 2x`
       )
+      expect(await browser.elementById('raw1').getAttribute('loading')).toBe(
+        'eager'
+      )
 
       expect(await browser.elementById('raw2').getAttribute('style')).toBe(
         'padding-left:4rem;width:100%;object-position:30% 30%'
@@ -725,6 +728,9 @@ function runTests(mode) {
       )
       expect(await browser.elementById('raw2').getAttribute('srcset')).toBe(
         `/_next/image?url=%2Fwide.png&w=16&q=75 16w, /_next/image?url=%2Fwide.png&w=32&q=75 32w, /_next/image?url=%2Fwide.png&w=48&q=75 48w, /_next/image?url=%2Fwide.png&w=64&q=75 64w, /_next/image?url=%2Fwide.png&w=96&q=75 96w, /_next/image?url=%2Fwide.png&w=128&q=75 128w, /_next/image?url=%2Fwide.png&w=256&q=75 256w, /_next/image?url=%2Fwide.png&w=384&q=75 384w, /_next/image?url=%2Fwide.png&w=640&q=75 640w, /_next/image?url=%2Fwide.png&w=750&q=75 750w, /_next/image?url=%2Fwide.png&w=828&q=75 828w, /_next/image?url=%2Fwide.png&w=1080&q=75 1080w, /_next/image?url=%2Fwide.png&w=1200&q=75 1200w, /_next/image?url=%2Fwide.png&w=1920&q=75 1920w, /_next/image?url=%2Fwide.png&w=2048&q=75 2048w, /_next/image?url=%2Fwide.png&w=3840&q=75 3840w`
+      )
+      expect(await browser.elementById('raw2').getAttribute('loading')).toBe(
+        'lazy'
       )
 
       expect(await browser.elementById('raw3').getAttribute('style')).toBeNull()
@@ -749,6 +755,85 @@ function runTests(mode) {
       }
     }
   })
+
+  it('should lazy load layout=raw and placeholder=blur', async () => {
+    const browser = await webdriver(appPort, '/layout-raw-placeholder-blur')
+
+    // raw1
+    expect(await browser.elementById('raw1').getAttribute('src')).toBe(
+      '/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.fab2915d.jpg&w=828&q=75'
+    )
+    expect(await browser.elementById('raw1').getAttribute('srcset')).toBe(
+      '/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.fab2915d.jpg&w=640&q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.fab2915d.jpg&w=828&q=75 2x'
+    )
+    expect(await browser.elementById('raw1').getAttribute('loading')).toBe(
+      'lazy'
+    )
+    expect(await browser.elementById('raw1').getAttribute('sizes')).toBeNull()
+    expect(await browser.elementById('raw1').getAttribute('style')).toMatch(
+      'filter:blur(20px);background-size:cover;'
+    )
+    expect(await browser.elementById('raw1').getAttribute('height')).toBe('400')
+    expect(await browser.elementById('raw1').getAttribute('width')).toBe('400')
+    await browser.eval(
+      `document.getElementById("raw1").scrollIntoView({behavior: "smooth"})`
+    )
+    await check(
+      () => browser.eval(`document.getElementById("raw1").currentSrc`),
+      /test(.*)jpg/
+    )
+    expect(await browser.elementById('raw1').getAttribute('src')).toBe(
+      '/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.fab2915d.jpg&w=828&q=75'
+    )
+    expect(await browser.elementById('raw1').getAttribute('srcset')).toBe(
+      '/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.fab2915d.jpg&w=640&q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.fab2915d.jpg&w=828&q=75 2x'
+    )
+    expect(await browser.elementById('raw1').getAttribute('loading')).toBe(
+      'lazy'
+    )
+    expect(await browser.elementById('raw1').getAttribute('sizes')).toBeNull()
+    expect(await browser.elementById('raw1').getAttribute('style')).toMatch('')
+    expect(await browser.elementById('raw1').getAttribute('height')).toBe('400')
+    expect(await browser.elementById('raw1').getAttribute('width')).toBe('400')
+
+    // raw2
+    expect(await browser.elementById('raw2').getAttribute('src')).toBe(
+      '/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=3840&q=75'
+    )
+    expect(await browser.elementById('raw2').getAttribute('srcset')).toBe(
+      '/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=384&q=75 384w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=640&q=75 640w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=750&q=75 750w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=828&q=75 828w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=1080&q=75 1080w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=1200&q=75 1200w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=1920&q=75 1920w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=2048&q=75 2048w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=3840&q=75 3840w'
+    )
+    expect(await browser.elementById('raw2').getAttribute('sizes')).toBe('50vw')
+    expect(await browser.elementById('raw2').getAttribute('loading')).toBe(
+      'lazy'
+    )
+    expect(await browser.elementById('raw2').getAttribute('style')).toMatch(
+      'filter:blur(20px);background-size:cover;'
+    )
+    expect(await browser.elementById('raw2').getAttribute('height')).toBe('400')
+    expect(await browser.elementById('raw2').getAttribute('width')).toBe('400')
+    await browser.eval(
+      `document.getElementById("raw2").scrollIntoView({behavior: "smooth"})`
+    )
+    await check(
+      () => browser.eval(`document.getElementById("raw2").currentSrc`),
+      /test(.*)png/
+    )
+    expect(await browser.elementById('raw2').getAttribute('src')).toBe(
+      '/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=3840&q=75'
+    )
+    expect(await browser.elementById('raw2').getAttribute('srcset')).toBe(
+      '/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=384&q=75 384w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=640&q=75 640w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=750&q=75 750w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=828&q=75 828w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=1080&q=75 1080w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=1200&q=75 1200w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=1920&q=75 1920w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=2048&q=75 2048w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=3840&q=75 3840w'
+    )
+    expect(await browser.elementById('raw2').getAttribute('sizes')).toBe('50vw')
+    expect(await browser.elementById('raw2').getAttribute('loading')).toBe(
+      'lazy'
+    )
+    expect(await browser.elementById('raw2').getAttribute('style')).toBe('')
+    expect(await browser.elementById('raw2').getAttribute('height')).toBe('400')
+    expect(await browser.elementById('raw2').getAttribute('width')).toBe('400')
+  })
+
   it('should handle the styles prop appropriately', async () => {
     let browser
     try {
@@ -859,6 +944,15 @@ function runTests(mode) {
       expect(await hasRedbox(browser)).toBe(true)
       expect(await getRedboxHeader(browser)).toMatch(
         /Image with src "(.*)bmp" has "placeholder='blur'" property but is missing the "blurDataURL" property/
+      )
+    })
+
+    it('should show error when layout=raw and lazyBoundary assigned', async () => {
+      const browser = await webdriver(appPort, '/invalid-raw-lazy-boundary')
+
+      expect(await hasRedbox(browser)).toBe(true)
+      expect(await getRedboxHeader(browser)).toContain(
+        `Image with src "/test.jpg" has "layout='raw'" and "lazyBoundary='500px'". For raw images, native lazy loading is used so "lazyBoundary" cannot be used.`
       )
     })
 

test/integration/image-component/default/pages/invalid-raw-lazy-boundary.js
@@ -1,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Page = () => {
+  return (
+    <div>
+      <Image
+        id="invalid-raw-lazy-boundary"
+        layout="raw"
+        src="/test.jpg"
+        width={200}
+        height={200}
+        lazyBoundary="500px"
+      />
+    </div>
+  )
+}
+
+export default Page

test/integration/image-component/default/pages/layout-raw-placeholder-blur.js
@@ -1,0 +1,27 @@
+import React from 'react'
+import Image from 'next/image'
+
+import testJPG from '../public/test.jpg'
+import testPNG from '../public/test.png'
+
+const Page = () => {
+  return (
+    <div id="container">
+      <h1>Layout Raw with Placeholder Blur</h1>
+      <p>Scroll down...</p>
+      <div style={{ height: '1000vh' }} />
+      <Image id="raw1" layout="raw" placeholder="blur" src={testJPG} />
+      <div style={{ height: '1000vh' }} />
+      <Image
+        id="raw2"
+        layout="raw"
+        placeholder="blur"
+        src={testPNG}
+        sizes="50vw"
+      />
+      <footer>Footer</footer>
+    </div>
+  )
+}
+
+export default Page

commit 0269a3a300799a654e6fa282852a2b636cf96bc5

Fix `next/image` using `layout=raw` with `priority` (#37381)
Fixes a regression from #36985 which switched to native lazy loading but didn't account for the case when the user sets `priority` which changes the default behavior of `loading` prop.

See https://github.com/vercel/next.js/pull/36985#issuecomment-1132267710

packages/next/client/image.tsx
@@ -457,7 +457,11 @@ export default function Image({
     unoptimized = true
     isLazy = false
   }
-  if (typeof window !== 'undefined' && loadedImageURLs.has(src)) {
+  if (
+    typeof window !== 'undefined' &&
+    loadedImageURLs.has(src) &&
+    layout !== 'raw'
+  ) {
     isLazy = false
   }
 
@@ -901,7 +905,7 @@ const ImageElement = ({
   blurStyle,
   isLazy,
   placeholder,
-  loading = 'lazy',
+  loading,
   srcString,
   config,
   unoptimized,
@@ -915,6 +919,7 @@ const ImageElement = ({
   noscriptSizes,
   ...rest
 }: ImageElementProps) => {
+  loading = isLazy ? 'lazy' : loading
   return (
     <>
       <img

test/integration/image-component/default/pages/priority.js
@@ -14,7 +14,7 @@ const Page = () => {
       ></Image>
       <Image
         loading="eager"
-        id="basic-image"
+        id="load-eager"
         src="/test.png"
         width="400"
         height="400"
@@ -37,7 +37,7 @@ const Page = () => {
       />
       <Image
         priority
-        id="raw"
+        id="raw1"
         src="/test.webp"
         width="1200"
         height="700"

test/integration/image-component/default/test/index.test.js
@@ -142,6 +142,23 @@ function runTests(mode) {
         },
       ])
 
+      // When priority={true}, we should _not_ set loading="lazy"
+      expect(
+        await browser.elementById('basic-image').getAttribute('loading')
+      ).toBe(null)
+      expect(
+        await browser.elementById('load-eager').getAttribute('loading')
+      ).toBe(null)
+      expect(
+        await browser.elementById('responsive1').getAttribute('loading')
+      ).toBe(null)
+      expect(
+        await browser.elementById('responsive2').getAttribute('loading')
+      ).toBe(null)
+      expect(await browser.elementById('raw1').getAttribute('loading')).toBe(
+        null
+      )
+
       const warnings = (await browser.log('browser'))
         .map((log) => log.message)
         .join('\n')